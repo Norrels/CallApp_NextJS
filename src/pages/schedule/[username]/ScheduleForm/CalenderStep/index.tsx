a04f00b5512498d8cd21b061ba716ea1e4b61b1d
Matheus Zacarias <matheuus412@gmail.com>
2023-01-13 15:00:44 -0300

    :sparkles: Nova pagina, o perfil com os dias disponiveis

---
+++ src/pages/schedule/[username]/ScheduleForm/CalenderStep/index.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Calendar } from "../../../../../Components/Calendar";
+import dayjs from 'dayjs'
+import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./style";
+
+export function CalendarStep() {
+    const [selectedDate, setSeletecDate] = useState<Date | null>(null)
+
+    const isDateSelected = !!selectedDate
+
+    const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+    const describeDate = selectedDate ? dayjs(selectedDate).format('DD[ de ]MMMM') : null
+
+    return (
+        <Container isTimePickerOpen={isDateSelected}>
+            <Calendar selectedDate={selectedDate} onDateSelected={setSeletecDate} />
+
+            {
+                isDateSelected && (
+                    <TimePicker>
+                        <TimePickerHeader>
+                            {weekDay} <span>{describeDate}</span>
+                        </TimePickerHeader>
+
+                        <TimePickerList>
+                            <TimePickerItem>08:00h</TimePickerItem>
+                            <TimePickerItem>09:00h</TimePickerItem>
+                            <TimePickerItem>10:00h</TimePickerItem>
+                            <TimePickerItem>11:00h</TimePickerItem>
+                            <TimePickerItem>12:00h</TimePickerItem>
+                            <TimePickerItem>13:00h</TimePickerItem>
+                            <TimePickerItem>14:00h</TimePickerItem>
+                        </TimePickerList>
+                    </TimePicker>
+                )
+            }
+
+
+
+
+        </Container>
+    )
+}
